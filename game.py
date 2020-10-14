9786162e93af5a53c256d2f2e3a9e88c7c488b0e
RidaLr <rida.laksir@usmba.ac.ma>
2020-10-14 22:51:58 +0200

    Add the game class file

---
+++ game.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 21:18:27 2020
+
+@author: Rida
+"""
+import pygame
+
+class Game(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        # Call the parent class (Sprite) constructor
+        super().__init__()    
