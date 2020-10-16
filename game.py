93bcd1adedc770ce8b7f7a822d0e58f01f97d379
RidaLr <rida.laksir@usmba.ac.ma>
2020-10-16 12:31:30 +0200

    Update the class game

--- game.py
+++ game.py
@@ -5,9 +5,19 @@ Created on Wed Oct 14 21:18:27 2020
 @author: Rida
 """
 import pygame
+from ball import Ball
+from brick import Brick
+from paddle import Paddle
+from player import Player
+
 
 class Game(pygame.sprite.Sprite):
     
     def __init__(self):
         # Call the parent class (Sprite) constructor
-        super().__init__()    
+        super().__init__()   
+        self.ball = Ball()
+        self.brick = Brick()
+        self.paddle = Paddle()
+        self.palyer = Player()
+        
