5e6951a8f717cc5fabf69e49f68975358f6f16b8
RidaLr <rida.laksir@usmba.ac.ma>
2020-10-16 12:32:03 +0200

    Update the main class

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ import pygame
 import math
 from player import Player
 from ball import Ball
+from game import Game
+from paddle import Paddle
+from brick import Brick
+
 
 pygame.init()
 
@@ -40,7 +44,20 @@ width = screen.get_width()
 # stores the height of the screen into a variable called height  
 height = screen.get_height() 
 
+#Load player
+player = Player()
 
+#Load game party
+game = Game()
+
+#Load ball
+ball = Ball()
+
+#Load brick
+brick = Brick()
+
+#Load paddle
+paddle = Paddle()
 
 running = True
 
@@ -51,6 +68,9 @@ while running:
     screen.blit(background, (0, 0))
     screen.blit(logo, (345, 150))
     screen.blit(play_button, play_button_rect)
+    screen.blit(game.ball.image, game.ball.rect)
+    screen.blit(game.brick.image, game.brick.rect)
+    screen.blit(game.paddle.image, game.paddle.rect)
     
     # else we will show the game interface and desable those images
     
@@ -79,6 +99,13 @@ while running:
                 #Start the game
                 #game.is_playing = True
                 print("start the game")
+                
+        #check if the player touch a key button
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                game.paddle.move_right()
+            if event.key == pygame.K_LEFT:
+                game.paddle.move_left()
                 
     
     # updates the frames of the game  
