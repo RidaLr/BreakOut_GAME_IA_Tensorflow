475feff7a81c8a514f98fd6ddb0f9459e039269d
RidaLr <rida.laksir@usmba.ac.ma>
2020-10-30 23:19:10 +0100

    Update the ball class

--- ball.py
+++ ball.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Oct 14 14:47:17 2020
 
-@author: Rida LAKSIR
+@author: Rida
 """
 
 import pygame
@@ -11,12 +11,26 @@ from random import randint
  
 class Ball(pygame.sprite.Sprite):
     
-    def __init__(self, color, width, height):
+    def __init__(self):
         # Call the parent class (Sprite) constructor
         super().__init__()       
         # Set the ball image
         self.image = pygame.image.load('assets/ball.png') 
-        # Fetch the rectangle object that has the dimensions of the image
         self.rect = self.image.get_rect()
-        # Game speed       
-        self.velocity = 5
+        self.radius = 10
+        # Game speed    
+        self.speed = 8
+        self.velocity = [randint(4,8),randint(-8,8)]
+        self.rect = self.image.get_rect()
+        self.rect.x = 540
+        self.rect.y = 550
+        
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+       
+          
+    def rebound(self):
+        self.velocity[0] = -self.velocity[0]
+        self.velocity[1] = randint(-8,8)
+
