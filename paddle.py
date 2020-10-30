74300297c746fb165c5447c8f131b14b3c656caf
RidaLr <rida.laksir@usmba.ac.ma>
2020-10-30 23:20:33 +0100

    Update paddle

--- paddle.py
+++ paddle.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Oct 15 22:30:09 2020
 
-@author: Rida LAKSIR
+@author: Rida
 """
 
 import pygame
@@ -14,21 +14,22 @@ class Paddle(pygame.sprite.Sprite):
     def __init__(self):
 
         super().__init__()
-        self.velocity = 50
+        self.velocity = 180
         self.image = pygame.image.load('assets/paddle.png')      
         self.rect = self.image.get_rect()
-        self.rect.x = 500
+        self.rect.x = 510
         self.rect.y = 580
+        self.direction = None
 
     def move_right(self):
         self.rect.x += self.velocity
-        
+        # Prevent the ball from going over the right wall
         if self.rect.x > width: 
           self.rect.x = width
 
     def move_left(self):
         self.rect.x -= self.velocity
-	    
+	    # Prevent the ball from going over the left wall
         if self.rect.x < 0:
           self.rect.x = 0
 
