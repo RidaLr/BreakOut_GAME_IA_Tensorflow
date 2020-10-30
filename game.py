7eb5650486f300e5cfdf41dcf357573d1476105e
RidaLr <rida.laksir@usmba.ac.ma>
2020-10-30 23:21:21 +0100

    Update the game file

--- game.py
+++ game.py
@@ -5,19 +5,177 @@ Created on Wed Oct 14 21:18:27 2020
 @author: Rida
 """
 import pygame
+import math
+import random
+
 from ball import Ball
 from brick import Brick
 from paddle import Paddle
 from player import Player
 
-
-class Game(pygame.sprite.Sprite):
+class Game:
     
     def __init__(self):
         # Call the parent class (Sprite) constructor
-        super().__init__()   
+        self.is_playing = False  
         self.ball = Ball()
-        self.brick = Brick()
         self.paddle = Paddle()
-        self.palyer = Player()
+        self.player = Player()
         
+    
+    def score(self, screen):
+        font = pygame.font.Font(None, 74)
+        score = font.render("Score: " + str(self.player.score), 1, (255, 255, 255))
+        screen.blit(score, (20,10))
+    
+    def lives(self,screen):
+        # draw score on the canvas
+        font = pygame.font.Font(None, 74)
+        lives = font.render("Lives: " + str(self.player.lives), 1,(255, 255, 255))
+        screen.blit(lives, (650,10))
+    
+    def game_over(self, screen):
+        
+        font = pygame.font.Font(None, 74)
+        game_over = font.render("GAME OVER", 1, (255, 255, 255))
+        screen.blit(game_over, (250,300))
+        
+        
+    def win_game(self, screen):
+        
+        font = pygame.font.Font(None, 74)
+        game_over = font.render("You win the game", 1, (255, 255, 255))
+        screen.blit(game_over, (250,300))
+        
+        
+    def draw_menu(self, screen, logo, play_button, play_button_rect):
+        screen.blit(logo, (345, 150))
+        screen.blit(play_button, play_button_rect)
+
+    def draw(self,all_bricks, brick_list, ball, paddle):
+        
+        all_sprites_list = pygame.sprite.Group()
+      
+        y = 60
+        for obj in brick_list:
+    
+            for i in range(14):
+                
+                brick = Brick(obj.image_src)
+                brick.rect.x = 10 + i* 75
+                brick.rect.y = y
+                all_sprites_list.add(brick)
+                all_bricks.add(brick)
+               
+            y += 27
+             
+        # Add the paddle to the list of sprites
+        all_sprites_list.add(paddle)
+        all_sprites_list.add(ball)
+        
+        
+        return all_sprites_list
+        
+
+    def play_music(self):
+        pygame.mixer.music.load("assets/music/ctr_ingame.mp3")
+        pygame.mixer.music.set_volume(0.4)
+        pygame.mixer.music.play(-1)
+        
+    def run(self, screen, background, all_sprites_list, logo, play_button, play_button_rect, paddle, ball, all_bricks):
+        running = True
+        self.play_music()
+        action = -1
+        while running:
+            
+            #stores the (x,y) coordinates into a tuple  
+            mouse = pygame.mouse.get_pos()
+            
+            # if the game not started yet we show those images
+            screen.blit(background, (0, 0))
+            
+            # else we will show the game interface and desable those images
+            if self.is_playing :
+                print("is player true")
+                all_sprites_list.draw(screen)
+                all_sprites_list.update()
+                
+                #Check if the ball is rebounding against any of the 4 walls:
+                if ball.rect.x>=1040:
+                    ball.velocity[0] = -ball.velocity[0]
+                if ball.rect.x<=0:
+                    ball.velocity[0] = -ball.velocity[0]
+                if ball.rect.y>580:
+                    ball.velocity[1] = -ball.velocity[1]
+                    self.player.lives -= 1
+                    if self.player.lives == 0:
+                        #Display Game Over Message
+                        self.game_over(screen)
+                        pygame.display.flip()
+                        # Wait 5 seconds before closing the game
+                        pygame.time.wait(5000)
+                        #Stop the Game
+                        running = False
+                
+                if ball.rect.y<40:
+                    ball.velocity[1] = -ball.velocity[1]
+            
+                #Detect collisions between the ball and the paddle
+                if pygame.sprite.collide_mask(ball, paddle):
+                  ball.rect.x -= ball.velocity[0]
+                  ball.rect.y -= ball.velocity[1]
+                  ball.rebound()
+                     
+                # Detect the collisions between the ball and the bricks 
+                brick_collision_list = pygame.sprite.spritecollide(ball,all_bricks,False)
+                for brick in brick_collision_list:
+                  ball.rebound()
+                  self.player.score += 1
+                  brick.kill()
+                  if len(all_bricks)==0:
+                        #Display 'you won the game' message
+                        self.win_game(screen)
+                        pygame.display.flip()
+                        # Wait 5 seconds before closing the game
+                        pygame.time.wait(5000)
+                        #Stop the Game
+                        running=False
+                
+                # Draw the score
+                self.score(screen)
+                # Draw the lives 
+                self.lives(screen)
+                
+                # updates the frames of the game  
+                pygame.display.update() 
+                
+                # The game is not already started, show the menu  
+            else:
+                self.draw_menu(screen, logo, play_button, play_button_rect)
+            
+            pygame.display.flip()
+                
+            #check if the mouse is clicked on the quit button in the quit button of the window
+            for event in pygame.event.get():
+                if(event.type == pygame.QUIT):
+                    running == False
+                    pygame.quit()
+            
+                #checks if a mouse is clicked  
+                if event.type == pygame.MOUSEBUTTONDOWN:  
+                      
+                    #if the mouse is clicked on the Play button the game will start  
+                    if (play_button_rect.collidepoint(event.pos)):
+                        #Start the game
+                        self.is_playing = True
+                        
+                #check if the player touch a key button
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        paddle.move_right()
+                    if event.key == pygame.K_LEFT:
+                        paddle.move_left()
+                        
+            
+        
+        pygame.quit()
